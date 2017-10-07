21f9d6757117cdeffe76289133d3f6f1df3639f7
Tom Drescher <tmdrescher@yahoo.com>
2017-10-07 15:24:27 -0400

    add function populateVoices

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,17 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#speak');
+const stopButton = document.querySelector('#stop');
 
-  const msg = new SpeechSynthesisUtterance();
-  let voices = [];
-  const voicesDropdown = document.querySelector('[name="voice"]');
-  const options = document.querySelectorAll('[type="range"], [name="text"]');
-  const speakButton = document.querySelector('#speak');
-  const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+
+
+function populateVoices() {
+  voices = this.getVoices();
+  voicesDropdown.innerHTML = voices
+    .map(voice => `<option value = "${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+}
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
